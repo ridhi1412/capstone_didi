e6f9edb30631b51fee1f4fb996123f9a8fc571e8
Ridhi Mahajan <ridhikmahajan@gmail.com>
2019-09-27 19:08:03 -0400

    experimenting with caches

--- loader1.py
+++ loader1.py
@@ -6,26 +6,48 @@ Created on Fri Sep 27 11:28:24 2019
 """
 
 import os
+import tarfile
 import pandas as pd
-from common import DATA_DIR
+from common import DATA_DIR, CACHE_DIR
 
-df_path = os.path.join(DATA_DIR, '2e65b3139cf147429f5b45b6b33e8807.tar.gz')
-df = pd.read_csv(df_path, skiprows=1, compression='gzip', header=None, error_bad_lines=False)
-df_index = list(df.index)
+#df_path = os.path.join(DATA_DIR, '2e65b3139cf147429f5b45b6b33e8807.tar.gz')
 
-df2 = pd.read_table(df_path, compression='gzip', header=None)
 
-df_not_index = list((set(list(range(len(df2))))).difference(set(df_index)))
 
+def load_all(use_cache=True):
+#    cache_path = my_path_join(CACHE_PATH, f'{buy_or_sell}_reader.msgpack')
+    for file in os.listdir(DATA_DIR):
+        file_path = os.path.join(DATA_DIR, file)
+        tar = tarfile.open(file_path, "r:gz")
+        for member in tar.getmembers():
+             cache_path = os.path.join(CACHE_DIR, f'{member.name}.msgpack')
+             print(member.name)
+             f = tar.extractfile(member)
+             if f is not None:
+                 df = pd.read_csv(f, header=None)
+                 pd.to_msgpack(cache_path, df)
+
+
+load_all()
+
+#df0 = df.iloc[0]
+#df1 = df.iloc[1:nan_index]
+#df2 = df.iloc[nan_index+1:]
 #
+#df1_ = df1[0].str.split(',', expand=True)
+##df2_ = df2[0].str.split(',', expand=True)
 #
-#df2 = 
+#a = df1.head(n=1000)
+#a = a.iloc[1:]
+#df1_ = a[0].str.split(',', expand=True)
 
-df2 = pd.read_table(df_path, compression='gzip', header=None)
-#aaa = df2.iloc[39829910:39829920]
-#bbb = aaa.isnull()
-#
-#xxx = df2.isnull()
-#
-#xxx = xxx.loc[xxx[0] == True]
-#aaa = df2.iloc[39829910:39829920]
+#import tarfile
+#tar = tarfile.open(df_path, "r:gz")
+#for member in tar.getmembers():
+#     print(member.name)
+##     df = pd.read_csv(df_path, sep='\t', compression='gzip', header=None)
+#     f = tar.extractfile(member)
+#     if f is not None:
+#         pass
+#         content = pd.read_csv(f, header=None)
+#         break
