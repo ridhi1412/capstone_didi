ed7ffe99c9f40532a3de50da4953976477605cc2
Akshat Mittal <aksmit94@gmail.com>
2019-09-28 00:09:37 -0400

    added utility functions and intialized notebook

--- common.py
+++ common.py
@@ -5,5 +5,5 @@ Created on Fri Sep 27 11:26:39 2019
 @author: rmahajan14
 """
 
-DATA_DIR = r'C:\Users\rmahajan14\capstone_data\data'
-CACHE_DIR = r'C:\Users\rmahajan14\capstone_data\cache'
+DATA_DIR = r'/mnt/d/DiDi_Ride_Data'
+CACHE_DIR = r'/mnt/d/DiDi_Ride_Data/Ride_Data_Cache'
