0f3ceed08fa9a4558503413f8fb16c3d4b6745a6
Ridhi Mahajan <rmahajan14@corp.bloomberg.com>
2019-10-06 15:21:12 -0400

    time categories by quantiles

--- yapf_format.py
+++ yapf_format.py
@@ -3,3 +3,4 @@ from yapf.yapflib.yapf_api import FormatFile  # reformat a file
 FormatFile('common.py', in_place=True)
 FormatFile('loader1.py', in_place=True)
 FormatFile('utils.py', in_place=True)
+FormatFile('experiment2.py', in_place=True)
