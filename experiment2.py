0b91b55dd38778a53923650815dd15ceb22f0d5b
Ashwin Jayaraman <ashwin@Ashwins-MacBook-Pro.local>
2019-10-18 14:23:57 -0400

    Resolving Conflicts

--- experiment2.py
+++ experiment2.py
@@ -1,14 +1,5 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Mon Sep 30 19:13:56 2019
 
-@author: rmahajan14
-"""
-from loader1 import read_data
-
-
-df_gps = read_data('gps', date='20161101', sample=1)
-df_order = read_data('order', date='20161101', sample=1)
 """
 Created on Sun Oct  6 14:11:27 2019
 
